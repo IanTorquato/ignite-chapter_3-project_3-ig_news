514e0cdbdb17ddb3ff752fd6bff2e7f0df3fb264
Ian Torquato <iantorquato2@gmail.com>
2021-07-20 22:14:26 -0300

    Adding SubscribeButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -20,6 +21,8 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+        <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
