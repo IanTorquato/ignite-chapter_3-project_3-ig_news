a095cecb2a1eacad8f2c0b2d3ccec4dc3efe910b
Ian Torquato <iantorquato2@gmail.com>
2021-07-21 21:40:11 -0300

    Integrating Github Authentication with Next Auth :tada:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import { Header } from '../components/Header';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>  
+    </NextAuthProvider>  
   );
 }
